c881fd5b9eadff17c45263a09ac02179d593d881
Jânderson Machado <jandersonmachado090@gmail.com>
2023-04-15 15:09:46 -0300

    add new file

---
+++ index.js
@@ -0,0 +1,47 @@
+document.querySelector('header').innerHTML = "Hello world"
+
+let javascript = document.getElementById("javascript");
+let novoParagrafo = document.createElement("p");
+novoParagrafo.textContent = "Hello world";
+javascript.appendChild(novoParagrafo);
+
+
+var h1 = document.createElement("h1");
+h1.textContent = "Hello world";
+document.body.appendChild(h1);
+
+var h2 = document.createElement("h2");
+h2.textContent = "Hello world"
+document.body.appendChild(h2);
+
+var h3 = document.createElement("h3");
+h3.textContent = "Hello world";
+document.body.appendChild(h3);
+
+var h4 = document.createElement("h4");
+h4.textContent = "Hello world";
+document.body.appendChild(h4)
+
+var h5 = document.createElement("h5");
+h5.textContent = "Hello world";
+document.body.appendChild(h5);
+
+var h6 = document.createElement("h6");
+h6.textContent = "Hello world";
+document.body.appendChild(h6);
+
+var strong = document.createElement("strong");
+strong.textContent = "Hello world";
+document.body.appendChild(strong);
+
+var li = document.createElement("li");
+li.textContent = "Hello world";
+document.body.appendChild(li);
+
+
+
+
+
+
+
+
